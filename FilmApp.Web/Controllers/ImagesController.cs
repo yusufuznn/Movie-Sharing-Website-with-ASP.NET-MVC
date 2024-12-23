7788c770e02434d2dc0cd708daf94a08aa5a3cca
Yusuf Uzun <Yusuf Uzun@DESKTOP-I9V17A6>
2024-12-24 00:45:13 +0300

    upload img-1 , integrate cloudinary

---
+++ FilmApp.Web/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmApp.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile file) 
+        {
+            // repository çağrısı
+
+        }
+    }
+}
---
+++ FilmApp.Web/Repositories/IImageRepository.cs
@@ -0,0 +1,7 @@
+﻿namespace FilmApp.Web.Repositories
+{
+    public interface IImageRepository
+    {
+        Task<string> UploadAsync(IFormFile file);
+    }
+}
