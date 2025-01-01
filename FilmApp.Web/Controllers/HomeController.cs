33051610da0f75805079bdcf5e2a658d59cb261d
Yusuf Uzun <Yusuf Uzun@DESKTOP-I9V17A6>
2025-01-01 13:53:13 +0300

    commit

--- FilmApp.Web/Controllers/HomeController.cs
+++ FilmApp.Web/Controllers/HomeController.cs
@@ -48,5 +48,9 @@ namespace FilmApp.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+
     }
 }
